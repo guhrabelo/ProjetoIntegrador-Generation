904958aee312959aeeb25d260db6dfe2ca177073
danilopereiradasilva <74926432+danilopereiradasilva@users.noreply.github.com>
2021-01-21 12:14:32 -0300

    Criação do Projeto Integrador usando spring boot

---
+++ LivrariaAbraco/src/test/java/com/recreio/LivrariaAbraco/LivrariaAbracoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recreio.LivrariaAbraco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaAbracoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
