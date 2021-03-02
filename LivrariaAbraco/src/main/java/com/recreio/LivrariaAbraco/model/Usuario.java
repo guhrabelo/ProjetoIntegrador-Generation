f7be3440d24f36313816859360ee78e4f242436f
guhrabelo <gustavo.rabelo.teles01@gmail.com>
2021-03-02 16:46:16 -0300

    atualização usuário

--- LivrariaAbraco/src/main/java/com/recreio/LivrariaAbraco/model/Usuario.java
+++ LivrariaAbraco/src/main/java/com/recreio/LivrariaAbraco/model/Usuario.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -22,6 +23,7 @@ public class Usuario
 	private String nome;
 	
 	@NotNull
+	@Email
 	private String email;
 	
 	@NotNull
